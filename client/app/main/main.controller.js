a05940c2779190195074c7b902f72e9f58f5c764
powolnymarcel <powolnymarcel@gmail.com>
2015-12-06 23:24:54 +0100

    mise en place API

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,6 @@
 
 angular.module('chopShopApp')
   .controller('PrincipalCtrl', function($scope, $http, socket, Produits) {
-    // Produits pour le nom du factory et query pour le nom de l'action contenue dans les resources
+    // 'Produits' pour le nom du factory et 'query()' pour le nom de l'action contenue dans les resources
     $scope.produits = Produits.query();
   });
