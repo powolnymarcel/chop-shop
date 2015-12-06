b9d777c86bdf0993e824f13aaa8e38a75a802cde
powolnymarcel <powolnymarcel@gmail.com>
2015-12-06 23:46:57 +0100

    mise en place API

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,8 @@
 
 angular.module('chopShopApp')
   .controller('PrincipalCtrl', function($scope, $http, socket, Produits) {
+
+    //Concerne la page d'accueil
     //On a besoin de tous les produits, on va faire appel au service "Produits" pour cela, on peut remarquer que le service a été injecté dans le controlleur
     // L'action query dans ressource utilise l'objet $http et fait un GET sur le serveur
     //Un GET sans :id sur le serveur active l'action "balanceMoiLaListeDeTousLesProduits" du controlleur serveur produits.controller.js
--- client/app/produits/produits.controller.js
+++ client/app/produits/produits.controller.js
@@ -2,7 +2,13 @@
 var errorHandler;
 angular.module('chopShopApp')
   .controller('ProduitsCtrl', function ($scope,Produits) {
-    $scope.message = 'Hello';
+
+    //On a besoin de tous les produits, on va faire appel au service "Produits" pour cela, on peut remarquer que le service a été injecté dans le controlleur
+    // L'action query dans ressource utilise l'objet $http et fait un GET sur le serveur
+    //Un GET sans :id sur le serveur active l'action "balanceMoiLaListeDeTousLesProduits" du controlleur serveur produits.controller.js
+
+    // 'Produits' pour le nom du factory et 'query()' pour le nom de l'action contenue dans les resources
+
     $scope.produits = Produits.query();
   })
 
